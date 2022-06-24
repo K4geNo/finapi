84cee504c59008d975133556c472135f0c5c2e9f
K4geNo <micaraujo71@gmail.com>
2022-06-24 16:02:23 -0300

    Validating existing CPF

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
-const express = require("express");
+const express = require('express')
+const { v4 } = require('uuid')
 
-const app = express();
+const app = express()
+
+app.use(express.json())
+
+const customers = []
+
+app.post('/account', (req, res) => {
+    const { cpf, name } = req.body
+
+    const customerAlreadyExists = customers.find(
+        (customer) => customer.cpf === cpf
+    )
+
+    if (customerAlreadyExists) {
+        return res.status(400).json({
+            error: 'Customer already exists',
+        })
+    }
+
+    customers.push({
+        id: v4(),
+        cpf,
+        name,
+        statement: [],
+    })
+
+    res.status(201).send()
+    console.log(customers)
+})
 
 app.listen(3333, () => {
-  console.log("Server started on port 3333");
-});
+    console.log('Server is running on port 3333')
+})
