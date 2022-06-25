8e35616921b7d2c9ededf2d6f33e0b9c4a19f593
K4geNo <micaraujo71@gmail.com>
2022-06-25 15:21:21 -0300

    Updating Account

--- src/index.js
+++ src/index.js
@@ -124,6 +124,22 @@ app.get('/statement/date', verifyIfExistsAccountCpf, (req, res) => {
     return res.json(statement)
 })
 
+app.put('/account', verifyIfExistsAccountCpf, (req, res) => {
+    const { name } = req.body
+
+    const { customer } = req
+
+    customer.name = name
+
+    return res.status(200).json(customer)
+})
+
+app.get('/account', verifyIfExistsAccountCpf, (req, res) => {
+    const { customer } = req
+
+    return res.json(customer)
+})
+
 app.listen(3333, () => {
     console.log('Server is running on port 3333')
 })
